4388c0ef73c46f4e66defcf34e21ca9b3b6d0a84
Nando Vieira <fnando.vieira@gmail.com>
2011-03-11 02:58:05 -0300

    Only require ActiveRecord if it's available

--- lib/permalink.rb
+++ lib/permalink.rb
@@ -4,5 +4,5 @@ require "permalink/orm/active_record"
 require "permalink/orm/mongo_mapper"
 require "permalink/orm/mongoid"
 
-ActiveRecord::Base.send(:include, Permalink::Orm::ActiveRecord)
+ActiveRecord::Base.send(:include, Permalink::Orm::ActiveRecord) if defined?(ActiveRecord)
 Mongoid::Document::ClassMethods.send(:include, Permalink::Orm::Mongoid) if defined?(Mongoid)
